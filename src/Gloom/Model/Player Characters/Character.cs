a9e29c55aeb7765b1b6edb0344f8158ebfcfeffb
B O'Connell <brendan.oconnell98@gmail.com>
2022-04-16 16:27:47 -0400

    Start Characters

--- src/Gloom/Model/Player Characters/Character.cs
+++ src/Gloom/Model/Player Characters/Character.cs
@@ -2,5 +2,87 @@
 
 public class Character
 {
-    
+    public Character(int maxHealth, int level, string name)
+    {
+        MaxHealth = maxHealth;
+        CurrentHealth = MaxHealth;
+        Level = level;
+        Name = name;
+        XP = 0;
+        Gold = 0;
+        Initiative = null;
+
+    }
+
+    public int CurrentHealth;
+    public int MaxHealth;
+    public int Level;
+    public string Name;
+    public int XP;
+    public int Gold;
+    public int? Initiative;
+
+    public void IncreaseCurrentHealthBy(int change)
+    {
+        if (change + CurrentHealth > MaxHealth)
+        {
+            CurrentHealth = MaxHealth;
+        }
+        else
+        {
+            CurrentHealth += change;
+        }
+    }
+
+    public void DecreaseCurrentHealthBy(int change)
+    {
+        if (change > CurrentHealth)
+        {
+            CurrentHealth = 0;
+        }
+        else
+        {
+            CurrentHealth -= change;
+        }
+    }
+    public void IncreaseXPBy(int change)
+    {
+            XP += change;
+    }
+
+    public void DecreaseXPBy(int change)
+    {
+        if (change > XP)
+        {
+            XP = 0;
+        }
+        else
+        {
+            XP -= change;
+        }
+
+    }
+
+    public void IncreaseGoldBy(int change)
+    {
+        Gold += change;
+    }
+
+    public void DecreaseGoldBy(int change)
+    {
+        if (change > Gold)
+        {
+            Gold = 0;
+        }
+        else
+        {
+            Gold -= change;
+        }
+
+    }
+
+    public void SetInitiative(int initiative) 
+    {
+        Initiative = initiative;
+    }
 }
---
+++ test/Gloom.Tests/CharacterTests.cs
@@ -0,0 +1,26 @@
+﻿using Gloom.Model.Player_Characters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Gloom.Tests
+{
+    public class CharacterTests
+    {
+        [Fact]
+        public void test()
+        {
+            int maxHealth = 58;
+            int level = 482;
+            string name = "Narge";
+            Character c = new Character(maxHealth, level, name);
+
+            c.DecreaseCurrentHealthBy(5);
+            c.SetInitiative(50);
+            int Init = c.Initiative.Value;
+        }
+    }
+}
