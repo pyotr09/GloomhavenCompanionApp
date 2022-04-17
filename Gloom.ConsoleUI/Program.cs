29221849a632ea7a2f2d48063630335dc6d44eeb
pyotr09 <peter.emanuel9@gmail.com>
2022-04-16 19:10:12 -0600

    I forgot the main class of the console project

---
+++ Gloom.ConsoleUI/Program.cs
@@ -0,0 +1,151 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Gloom.Model.Monsters;
+using Gloom.Model.Scenario;
+
+Scenario scenario = null;
+
+var addScenario = () =>
+{
+    Console.Write("Level: ");
+    var levelText = Console.ReadLine();
+    int level;
+    while (!int.TryParse(levelText, out level))
+    {
+        Console.Write("Not an integer, try again: ");
+        levelText = Console.ReadLine();
+    }
+    
+    Console.Write("Number (-1 for custom): ");
+    var numText = Console.ReadLine();
+    int num;
+    while (!int.TryParse(numText, out num))
+    {
+        Console.Write("Not an integer, try again: ");
+        numText = Console.ReadLine();
+    }
+
+    if (num == -1)
+    {
+        Console.Write("Name: ");
+        var name = Console.ReadLine();
+        scenario = new Scenario(level, name);
+    }
+    else
+    {
+        scenario = new Scenario(level, num, "Gloomhaven");
+    }
+    
+    return scenario.ToString();
+};
+
+var addMonsterGroup = () =>
+{
+    if (scenario == null)
+        return "Add a scenario first! Use \"add scenario\" command";
+    
+    Console.Write("Name: ");
+    var name = Console.ReadLine();
+    
+    Console.Write("Name of Ability Deck: ");
+    var deckName = Console.ReadLine();
+    
+    scenario.AddMonsterGroup(name, deckName);
+    return scenario.ToString();
+};
+
+var addMonster = () =>
+{
+    if (scenario == null)
+        return "Add a scenario first! Use \"add scenario\" command";
+    if (scenario.Participants.Count == 0)
+        return "Add a monster group first! Use \"add group\" command";
+
+    string groupText;
+    if (scenario.Participants.Count == 1)
+    {
+        groupText = scenario.Participants.First().Name;
+    }
+    else
+    {
+        var groupNames = scenario.Participants
+            .Where(a => a is MonsterGrouping)
+            .Select(g => g.Name).ToList();
+        var namesString = string.Join(",", groupNames);
+        do
+        {
+            Console.Write(
+                $"Which group [{namesString}]? ");
+            groupText = Console.ReadLine() ?? string.Empty;
+        } while (!groupNames.Contains(groupText));
+    }
+
+    string ne;
+    do
+    {
+        Console.Write("Normal/Elite? (Enter n or e): ");
+        ne = Console.ReadLine() ?? string.Empty;
+    } while (ne != "n" && ne != "e");
+
+    var tier = ne == "n" ? MonsterTier.Normal : MonsterTier.Elite;
+    (scenario.Participants.First(g => g.Name == groupText) as MonsterGrouping)?.AddMonster(tier);
+
+    return scenario.ToString();
+};
+
+var draw = () =>
+{
+    if (scenario == null)
+        return "Add a scenario first! Use \"add scenario\" command";
+    if (scenario.Participants.Count == 0)
+        return "Add a monster group first! Use \"add group\" command";
+    if (scenario.Participants.Where(g => g is MonsterGrouping)
+        .All(g => (g as MonsterGrouping).Monsters.Count == 0))
+    {
+        return "Add a monster first! Use \"add monster\" command";
+    }
+    
+    scenario.Draw();
+    
+    return scenario.ToString();
+};
+
+Console.WriteLine("Hello, World!");
+string? commandText;
+do
+{
+    Console.Write("Enter Command: ");
+    commandText = Console.ReadLine();
+    string output = "";
+
+    try
+    {
+        if (commandText == "add scenario")
+        {
+            output = addScenario();
+        }
+
+        if (commandText == "add group")
+        {
+            output = addMonsterGroup();
+        }
+
+        if (commandText == "add monster")
+        {
+            output = addMonster();
+        }
+
+        if (commandText == "draw")
+        {
+            output = draw();
+        }
+    }
+    catch (Exception e)
+    {
+        output = e.Message + "\n" + e.StackTrace;
+    }
+
+    Console.WriteLine(output);
+} while(commandText != "exit");
+
+Console.WriteLine("Goodbye, World!");
